8ec7257e112ede9b932aaf0fb048397fc32f0c72
danailch456 <danailchaushev@gmail.com>
2019-06-13 21:11:14 +0300

    1.0 release

--- albion/bin/configdb.js
+++ albion/bin/configdb.js
@@ -4,13 +4,13 @@ configSQL = `drop database if exists albion;
 create database albion;
 use albion;
 create table item (id integer not null auto_increment primary key,classification varchar(60),tier integer not null,name varchar(60) not null,price integer not null);
-create table city (id integer not null auto_increment primary key,name varchar(60) not null);
+create table city (id integer not null auto_increment primary key,cname varchar(60) not null);
 create table market (itemId integer not null,cityId integer not null,foreign key (itemId) references item(id),foreign key (cityId) references city(id));
-insert into city(name) values ("Bridgewatch");
-insert into city(name) values ("Carleon");
-insert into city(name) values ("Martlock");
-insert into city(name) values ("Thetford");
-insert into city(name) values ("Lymhurst");`;
+insert into city(cname) values ("Bridgewatch");
+insert into city(cname) values ("Carleon");
+insert into city(cname) values ("Martlock");
+insert into city(cname) values ("Thetford");
+insert into city(cname) values ("Lymhurst");`;
 
 var connection = mysql.createConnection({
 	host: 'localhost',
--- albion/modules/Item.js
+++ albion/modules/Item.js
@@ -1,57 +1,98 @@
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dani',
+    password: 'root',
+});
+
+connection.connect((err) => {
+    if (err) {
+        throw err
+    } else {
+        console.log('database connected...');
+    }
+});
+
+connection.query(`USE albion`);
 //adds new items to database getting the data from req.body
-exports.Create = ()=>{
-    return (req,res,next)=>{
-        body = req.body;
-        connection.query(SQL.create(body), function (err) {
-            if (err){
+exports.Create = () => {
+    return (req, res, next) => {
+        ({ type, tier, name, city, price } = req.body);
+        var id;
+        connection.query(`Select c.id from city c where c.cname = '${city}'`, (err, results, fields) => {
+            if (err) {
                 console.log(err);
-                res.status(500);
-            } 
-            else res.status(200);
+            } else {
+                city = results[0].id;
+                connection.query(`insert into item(classification, tier, name, price) values('${type}', ${tier}, '${name}', ${price});`, function (err, results, fields) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        id = results.insertId;
+                        connection.query(`insert into market(itemId, cityId) values(${id}, '${city}');`, function (err, results, fields) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else{
+                                res.status(200).json({id}).end();
+                            }
+                        });
+                    }
+                });
+            }
         });
-    }    
+
+    }
 };
 
-//gets new items from database sorted by price
-exports.Get = ()=>{
-    return (req,res,next)=>{
+//gets new items from database sorted by field of choice
+//tier || price || name || classification
+exports.Get = () => {
+    return (req, res, next) => {
         sortingField = req.body.sortingField;
-        connection.query(SQL.get(sortingField), function (err,results,fields) {
-            if (err){
+        connection.query(`select i.classification, i.tier, i.name, i.price, c.cname
+            from market m
+            left join item i On m.itemId = i.Id
+            left join city c on m.cityId = c.id
+            order by ${sortingField};`, function (err, results, fields) {
+            if (err) {
                 console.log(err);
                 res.status(500);
             }
-            else res.status(200).json({results:results});
-        });   
+            else res.status(200).json({results});
+        });
     }
 }
 
-//updates the price of items from database according to data from the body(single field "silver":<number>)
-exports.Update = ()=>{
-    return (req,res,next)=>{
-        price = req.body.price;
-        connection.query(SQL.update(price), function (err) {
-            if (err){
+//updates the price of items from database according to data from the body("price":<number>,"id":<number>)
+exports.Update = () => {
+    return (req, res, next) => {
+        ({ id, price } = req.body);
+        connection.query(`update item set item.price=${price} 
+        where item.id=${id};`, function (err) {
+            if (err) {
                 console.log(err);
                 res.status(500);
-            } 
+            }
             else res.status(200);
         });
-        
+
     }
 }
 
-//deletes the item from the store
-exports.Delete = ()=>{
-    return (req,res,next)=>{
+//deletes the item from the database
+exports.Delete = () => {
+    return (req, res, next) => {
         id = req.body.id;
-        connection.query(SQL.delete(id), function (err) {
-            if (err){
+        connection.query(`delete from market 
+        where itemId = ${id};`, function (err,results,fields) {
+            if (err) {
                 console.log(err);
                 res.status(500);
-            } 
-            else res.status(200);
+            }
+            else res.status(200).json({deleted : true,id : id});
         });
     }
 }
